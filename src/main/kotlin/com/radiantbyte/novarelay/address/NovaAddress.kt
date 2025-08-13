00cb2fa7767670b4dd246f8c96cd85f4ba69af8b
RadiantByte <kazimishal2006@gmail.com>
2025-08-13 07:15:17 +0600

    Gaichū kujo-chū

--- src/main/kotlin/com/radiantbyte/novarelay/address/NovaAddress.kt
+++
@@ -1,9 +0,0 @@
-package com.radiantbyte.novarelay.address
-
-import java.net.InetSocketAddress
-
-data class NovaAddress(val hostName: String, val port: Int)
-
-inline val NovaAddress.inetSocketAddress
-    get() = InetSocketAddress(hostName, port)
-
